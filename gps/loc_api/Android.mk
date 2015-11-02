9c04de140ebf07804c02e80ac478ea6cd7d2eca7
Michael Bestas <mikeioannina@gmail.com>
2015-11-02 04:47:11 +0200

    ham: Remove loc_api that depends on QCPATH
    * Also cleanup the makefiles while we are at it
    
    Change-Id: I8799ca9e0f9e0a3952997a21d6d4d1619aad86fa
    Signed-off-by: sb6596 <shubhamprince111@gmail.com>

--- gps/loc_api/Android.mk
+++ gps/loc_api/Android.mk
@@ -1,10 +1,11 @@
-MY_PATH := $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 
 # add RPC dirs if RPC is available
 ifneq ($(TARGET_NO_RPC),true)
-include $(MY_PATH)/libloc_api-rpc-50001/Android.mk
+include $(LOCAL_PATH)/libloc_api-rpc-50001/Android.mk
 endif #TARGET_NO_RPC
 
-include $(MY_PATH)/libloc_api_50001/Android.mk
-include $(MY_PATH)/loc_api_v02/Android.mk
-include $(MY_PATH)/ds_api/Android.mk
+GPS_DIR_LIST += $(LOCAL_PATH)/libloc_api_50001/
+
+#call the subfolders
+include $(addsuffix Android.mk, $(GPS_DIR_LIST))
