2187ce1c711ccd14563a0ad4544c8a3c3c3789c4
Bruno Martins <bgcngm@gmail.com>
2017-12-16 22:09:09 +0000

    msm8974-common: Add common compatibility matrix
    Change-Id: I54085a73e48519f1980066c10b54a8783967d362
    Signed-off-by: sb6596 <shubhamprince111@gmail.com>
    Signed-off-by: ShubhamB <shubhamprince111@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -129,6 +129,7 @@ MAX_EGL_CACHE_SIZE 	:= 2048*1024
 
 # HIDL
 DEVICE_MANIFEST_FILE 	:= $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE   	:= $(DEVICE_PATH)/compatibility_matrix.xml
 
 #HWUI
 HWUI_COMPILE_FOR_PERF 	:= true
