ceb987c42b44b7e07693a0ba165a76f81ccce774
Paul Keith <javelinanddart@gmail.com>
2018-02-19 17:51:32 +0000

    ham: Use sdFAT for exFAT and vFAT
    Signed-off-by: ShubhamB <shubhamprince111@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -215,7 +215,7 @@ ifeq ($(HOST_OS),linux)
 endif
 
 # Extended filesystem support
-TARGET_EXFAT_DRIVER := exfat
+TARGET_EXFAT_DRIVER := sdfat
 
 # Use Snapdragon LLVM Compiler
 TARGET_USE_SDCLANG := true
