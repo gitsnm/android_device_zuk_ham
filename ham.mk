80a40d4617db229151d10fea388aca6a27c8bf55
Adithya R <gh0strider.2k18.reborn@gmail.com>
2018-04-22 14:58:26 +0000

    ham: Minor tweaks to dalvik
    Change-Id: I00996fe9ca5184106076874652972bc9e26602b4

--- ham.mk
+++ ham.mk
@@ -305,12 +305,15 @@ PRODUCT_COPY_FILES += \
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.checkjni=false
     dalvik.vm.heapstartsize=16m \
     dalvik.vm.heapgrowthlimit=192m \
     dalvik.vm.heapsize=512m \
     dalvik.vm.heaptargetutilization=0.75 \
     dalvik.vm.heapminfree=2m \
     dalvik.vm.heapmaxfree=8m
+    dalvik.vm.verify-bytecode=false
+    persist.sys.purgeable_assets=1
 
 
 # HWUI
