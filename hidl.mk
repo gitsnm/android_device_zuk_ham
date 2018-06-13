04360be44a4beadc75bc1f52bf78276fd2e6aa11
sb6596 <shubhamprince111@gmail.com>
2018-06-13 13:13:03 +0000

    ham: Use custom biometrics that properly supports our device
    * Also remove un-needed wrapper
    * Also add context for custom biometrics & arrange file_contexts
    
    Signed-off-by: sb6596 <shubhamprince111@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -46,7 +46,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.0-service.ham
 
 # Graphics
 PRODUCT_PACKAGES += \
