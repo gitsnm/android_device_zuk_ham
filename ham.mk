11daaca9b7911852c23eba4ef886d855c90ffd96
Shoaib <shoaib.gcpdev@gmail.com>
2018-06-21 11:19:41 +0000

    ham : add WallpaperPicker

--- ham.mk
+++ ham.mk
@@ -347,6 +347,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.hwui.text_large_cache_width=2048 \
     ro.hwui.text_large_cache_height=1024
 
+#WallpaperPicker
+PRODUCT_PACKAGES += \
+    WallpaperPicker
+
 # call dalvik heap config
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
 
