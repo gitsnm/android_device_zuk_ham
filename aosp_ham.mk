f2251fb86f0206356f4ef026a144406ae11fa475
ShubhamB <shubhamprince111@gmail.com>
2018-08-14 10:40:10 +0000

    ham: Pixel-P
    Signed-off-by: ShubhamB <shubhamprince111@gmail.com>

--- aosp_ham.mk
+++ aosp_ham.mk
@@ -19,14 +19,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from ham device
 $(call inherit-product, device/zuk/ham/ham.mk)
 
-# Inherit some common AEX stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
-
-# AospExtended-BuildType
-EXTENDED_BUILD_TYPE := OFFICIAL
 
 PRODUCT_NAME := aosp_ham
 PRODUCT_DEVICE := ham
