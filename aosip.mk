c215f2c8c5d5994a0f0eb8dff2480476e5ec3b84
ShoaibM <shoaib.pi@gmail.com>
2018-10-07 16:23:48 +0000

    ham: AOSiP bringup

--- aosip.mk
+++ aosip.mk
@@ -20,9 +20,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/zuk/ham/ham.mk)
 
 # Inherit some common AOSP stuff.
-$(call inherit-product, vendor/potato/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
-PRODUCT_NAME := potato_ham
+PRODUCT_NAME := aosip_ham
 PRODUCT_DEVICE := ham
 PRODUCT_MANUFACTURER := ZUK
 PRODUCT_MODEL := ZUK Z1
