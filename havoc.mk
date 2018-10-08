d3abda77964e720f20b10134e0bf7b6601c8db5a
ShoaibM <shoaib.pi@gmail.com>
2018-10-08 17:57:57 +0000

    ham : Create HAVOC!

--- havoc.mk
+++ havoc.mk
@@ -20,9 +20,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/zuk/ham/ham.mk)
 
 # Inherit some common AOSP stuff.
-$(call inherit-product, vendor/potato/config/common_full_phone.mk)
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
-PRODUCT_NAME := potato_ham
+PRODUCT_NAME := havoc_ham
 PRODUCT_DEVICE := ham
 PRODUCT_MANUFACTURER := ZUK
 PRODUCT_MODEL := ZUK Z1
